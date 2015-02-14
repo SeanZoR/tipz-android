0b6cf4f398629e0dd1286fdde5ca7a16e2bb50b8
Sean Katz <sean.katz@gmail.com>
2015-02-14 18:46:37 +0200

    Adding a MainActivity with drawer and material design behaviours

--- app/src/main/java/com/tipz/app/view/LauncherActivity.java
+++ app/src/main/java/com/tipz/app/view/LauncherActivity.java
@@ -1,8 +1,10 @@
 package com.tipz.app.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 
 import com.tipz.app.R;
 import com.tipz.app.TipzApplication;
@@ -22,5 +24,9 @@ public class LauncherActivity extends Activity {
         // Update the number of application launches
         mApp.getPrefs().applyInt(R.string.pref_app_launch_times,
                 mApp.getPrefs().getInt(R.string.pref_app_launch_times, 0) + 1);
+
+        // Start the main activity
+        startActivity(new Intent(this, MainActivity.class));
+        this.finish();
     }
 }
---
+++ app/src/main/java/com/tipz/app/view/MainActivity.java
@@ -0,0 +1,64 @@
+package com.tipz.app.view;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+
+import com.tipz.app.R;
+import com.tipz.app.TipzApplication;
+
+public class MainActivity extends BaseActivity<TipzApplication>
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout),
+                mToolbar);
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // Nothing for now
+    }
+
+    public void restoreActionBar() {
+        mToolbar.setTitle(mTitle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected int getActivityLayout() {
+        return R.layout.activity_main;
+    }
+}
