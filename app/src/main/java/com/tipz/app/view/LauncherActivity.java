dc023ff3e15097d0a7a73146577c6a8124739fde
Sean Katz <sean.katz@gmail.com>
2015-02-14 13:23:57 +0200

    Adding reference from the launcher activity to the application class

--- app/src/main/java/com/tipz/app/view/LauncherActivity.java
+++ app/src/main/java/com/tipz/app/view/LauncherActivity.java
@@ -1,7 +1,23 @@
 package com.tipz.app.view;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.tipz.app.TipzApplication;
 
 public class LauncherActivity extends Activity {
+
+    protected final String TAG = ((Object) this).getClass().getSimpleName();
+
+    protected TipzApplication mApp;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        mApp = (TipzApplication) getApplicationContext();
+    }
+
 
 }
